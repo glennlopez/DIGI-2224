df8ed35e8ab38451cf3ba5e4fa67c6a0ead3d1b3
glennlopez <glennlopez@gmail.com>
2015-02-04 12:56:14 -0600

    rewrote mux_8ch

--- Unit 2 - VHDL Decoders and Multiplexers/LAB_4/mux_8ch/mux_8ch.vhd
+++ Unit 2 - VHDL Decoders and Multiplexers/LAB_4/mux_8ch/mux_8ch.vhd
@@ -5,25 +5,23 @@ use ieee.std_logic_1164.all;
 --entity
 entity mux_8ch is
 	port
-	(
-		--mus = 8 data lines | 3 select lines | 1 output
-		--data line							 	7654321
-		d:		in			std_logic_vector(7 downto 0);
-		
-		--select line						  	210
-		s:		in			std_logic_vector(2 downto 0); ar
-		
-		--output
-		y:		out		std_logic
+	(					 	7654321
+		d:		in			std_logic_vector(7 downto 0);		--this is where data comes from 
+		s:		in			std_logic_vector(2 downto 0);		--this will select which data flows
+		y:		out		std_logic								--this is where data comes out
 	);
 end mux_8ch;
 
 --architecture
 architecture logic_8to1 of mux_8ch is
---concatinate signal
-signal:		input		(2 downto 0);
 begin
-	-- select statement include select line
-	with input select
-	
+	with s select
+		Y		<=		D0 when "000",
+						D1 when "001",
+						D2 when "010",
+						D3 when "011",
+						D4 when "100",
+						D5 when "101",
+						D6 when "110",
+						D7 when "111";
 end logic_8to1;
