6cf60214d2d9fe9a1993c19470249a6b7ac0d03c
glennlopez <glennlopez@gmail.com>
2015-02-04 14:31:00 -0600

    Addded gitignore to new folder

--- Unit 2 - VHDL Decoders and Multiplexers/LAB_4/mux_8ch/mux_8ch.vhd
+++ Unit 2 - VHDL Decoders and Multiplexers/LAB_4/mux_8ch/mux_8ch.vhd
@@ -1,3 +1,10 @@
+----------------------------------------------------------------------
+--			LAB4: mux_8ch.vhd
+--
+--			Write a VHDL file, using a selected signal assignment 
+--			statement, to design an 8-to-1 multiplexer.
+----------------------------------------------------------------------
+
 -- library
 library ieee;
 use ieee.std_logic_1164.all;
@@ -5,10 +12,17 @@ use ieee.std_logic_1164.all;
 --entity
 entity mux_8ch is
 	port
-	(					 	7654321
-		d:		in			std_logic_vector(7 downto 0);		--this is where data comes from 
-		s:		in			std_logic_vector(2 downto 0);		--this will select which data flows
-		y:		out		std_logic								--this is where data comes out
+	(	
+		d0: in std_logic;
+		d1: in std_logic;
+		d2: in std_logic;
+		d3: in std_logic;
+		d4: in std_logic;
+		d5: in std_logic;
+		d6: in std_logic;
+		d7: in std_logic;
+		s:	in std_logic_vector(2 downto 0);
+		y:	out std_logic
 	);
 end mux_8ch;
 
@@ -16,7 +30,7 @@ end mux_8ch;
 architecture logic_8to1 of mux_8ch is
 begin
 	with s select
-		Y		<=		D0 when "000",
+		y		<=		D0 when "000",
 						D1 when "001",
 						D2 when "010",
 						D3 when "011",
