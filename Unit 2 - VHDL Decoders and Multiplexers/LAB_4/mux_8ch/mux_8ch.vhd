9bf19853fa1417077310bc17075b2ac92b2341de
glennlopez <glennlopez@gmail.com>
2015-02-04 20:08:31 -0600

    fixed code glitch

--- Unit 2 - VHDL Decoders and Multiplexers/LAB_4/mux_8ch/mux_8ch.vhd
+++ Unit 2 - VHDL Decoders and Multiplexers/LAB_4/mux_8ch/mux_8ch.vhd
@@ -13,14 +13,7 @@ use ieee.std_logic_1164.all;
 entity mux_8ch is
 	port
 	(	
-		d0: in std_logic;
-		d1: in std_logic;
-		d2: in std_logic;
-		d3: in std_logic;
-		d4: in std_logic;
-		d5: in std_logic;
-		d6: in std_logic;
-		d7: in std_logic;
+		d: in std_logic_vector(0 to 7);
 		s:	in std_logic_vector(2 downto 0);
 		y:	out std_logic
 	);
@@ -30,12 +23,14 @@ end mux_8ch;
 architecture logic_8to1 of mux_8ch is
 begin
 	with s select
-		y		<=		D0 when "000",
-						D1 when "001",
-						D2 when "010",
-						D3 when "011",
-						D4 when "100",
-						D5 when "101",
-						D6 when "110",
-						D7 when "111";
+	-- COMMON ERROR: 	Do not mistake d(0) for d0. These are not the same.
+	--						d(0) points to a vector, d0 points to a port.
+		y		<=	  	d(0) when "000",
+						d(1) when "001",
+						d(2) when "010",
+						d(3) when "011",
+						d(4) when "100",
+						d(5) when "101",
+						d(6) when "110",
+						d(7) when "111";
 end logic_8to1;
