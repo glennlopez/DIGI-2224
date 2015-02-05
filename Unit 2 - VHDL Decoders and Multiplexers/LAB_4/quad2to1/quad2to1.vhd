9b68e6ba1cb2cb4cc3233797912ae0402200984d
glennlopez <glennlopez@gmail.com>
2015-02-05 08:04:34 -0600

    checkpoint - started lab 4 quad2to1

---
+++ Unit 2 - VHDL Decoders and Multiplexers/LAB_4/quad2to1/quad2to1.vhd
@@ -0,0 +1,19 @@
+----------------------------------------------------------------------
+--			LAB4: quad2to1.vhd
+--
+--		Write a VHDL file that defines a multiplexer that switches 
+--		two 4-bit inputs, x and y to a 4-bit output, z. Define x, y, 
+--		and z as type BIT_VECTOR
+----------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+--entity
+entity quad2to1 is
+  port (
+	
+  );
+end entity;
+
+--architecture
+
